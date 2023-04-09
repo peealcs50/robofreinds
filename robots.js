7d1eba06e5570ba4d099e425e590c6e341c3a9b0
peealcs50 <121597813+peealcs50@users.noreply.github.com>
2023-04-09 12:57:23 -0400

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,305 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    },
+    /*
+    {
+        id: 11,
+        name: 'Andrew Thompson',
+        username: 'andrewt',
+        email: 'andrewt@example.com'
+      },
+      {
+        id: 12,
+        name: 'Grace Martin',
+        username: 'gracem',
+        email: 'gracem@example.com'
+      },
+      {
+        id: 13,
+        name: 'Oliver Turner',
+        username: 'olivert',
+        email: 'olivert@example.com'
+      },
+      {
+        id: 14,
+        name: 'Sophie Wright',
+        username: 'sophiew',
+        email: 'sophiew@example.com'
+      },
+      {
+        id: 15,
+        name: 'Charles Davis',
+        username: 'charlesd',
+        email: 'charlesd@example.com'
+      },
+      {
+        id: 16,
+        name: 'Anna Garcia',
+        username: 'annag',
+        email: 'annag@example.com'
+      },
+      {
+        id: 17,
+        name: 'Michael Clark',
+        username: 'michaelc',
+        email: 'michaelc@example.com'
+      },
+      {
+        id: 18,
+        name: 'Lucy Hall',
+        username: 'lucyh',
+        email: 'lucyh@example.com'
+      },
+      {
+        id: 19,
+        name: 'William Lee',
+        username: 'williaml',
+        email: 'williaml@example.com'
+      },
+      {
+        id: 20,
+        name: 'Emma Hernandez',
+        username: 'emmah',
+        email: 'emmah@example.com'
+      },
+      {
+        id: 20,
+        name: 'Emma Hernandez',
+        username: 'emmah',
+        email: 'emmah@example.com'
+      },
+      {
+        id: 21,
+        name: 'Liam Foster',
+        username: 'liamf',
+        email: 'liamf@example.com'
+      },
+      {
+        id: 22,
+        name: 'Ava Brooks',
+        username: 'avab',
+        email: 'avab@example.com'
+      },
+      {
+        id: 23,
+        name: 'Noah Parker',
+        username: 'noahp',
+        email: 'noahp@example.com'
+      },
+      {
+        id: 24,
+        name: 'Sophia Ellis',
+        username: 'sophiae',
+        email: 'sophiae@example.com'
+      },
+      {
+        id: 25,
+        name: 'Ethan Harrison',
+        username: 'ethanh',
+        email: 'ethanh@example.com'
+      },
+      {
+        id: 26,
+        name: 'Isabella Davis',
+        username: 'isabellad',
+        email: 'isabellad@example.com'
+      },
+      {
+        id: 27,
+        name: 'Aiden Scott',
+        username: 'aidens',
+        email: 'aidens@example.com'
+      },
+      {
+        id: 28,
+        name: 'Mia Robinson',
+        username: 'miar',
+        email: 'miar@example.com'
+      },
+      {
+        id: 29,
+        name: 'Carter Kelly',
+        username: 'carterk',
+        email: 'carterk@example.com'
+      },
+      {
+        id: 29,
+        name: 'Olivia Turner',
+        username: 'oliviat',
+        email: 'oliviat@example.com'
+      },
+      {
+        id: 30,
+        name: 'Michaela King',
+        username: 'michaelak',
+        email: 'michaelak@example.com'
+      },
+      {
+        id: 31,
+        name: 'Ethan Green',
+        username: 'ethang',
+        email: 'ethang@example.com'
+      },
+      {
+        id: 32,
+        name: 'Hannah Lee',
+        username: 'hannahl',
+        email: 'hannahl@example.com'
+      },
+      {
+        id: 33,
+        name: 'William Adams',
+        username: 'williama',
+        email: 'williama@example.com'
+      },
+      {
+        id: 34,
+        name: 'Abigail Mitchell',
+        username: 'abigailm',
+        email: 'abigailm@example.com'
+      },
+      {
+        id: 35,
+        name: 'James Perez',
+        username: 'jamesp',
+        email: 'jamesp@example.com'
+      },
+      {
+        id: 36,
+        name: 'Madison Carter',
+        username: 'madisonc',
+        email: 'madisonc@example.com'
+      },
+      {
+        id: 37,
+        name: 'David Turner',
+        username: 'davidt',
+        email: 'davidt@example.com'
+      },
+      {
+        id: 38,
+        name: 'Elizabeth Martin',
+        username: 'elizabethm',
+        email: 'elizabethm@example.com'
+      },
+      {
+        id: 39,
+        name: 'Logan Hill',
+        username: 'loganh',
+        email: 'loganh@example.com'
+      },
+      {
+        id: 40,
+        name: 'Isabella Campbell',
+        username: 'isabellac',
+        email: 'isabellac@example.com'
+      },
+      {
+        id: 41,
+        name: 'Samuel Parker',
+        username: 'samuelp',
+        email: 'samuelp@example.com'
+      },
+      {
+        id: 42,
+        name: 'Avery Nelson',
+        username: 'averyn',
+        email: 'averyn@example.com'
+      },
+      {
+        id: 43,
+        name: 'Victoria Phillips',
+        username: 'victoriap',
+        email: 'victoriap@example.com'
+      },
+      {
+        id: 44,
+        name: 'Daniel Martinez',
+        username: 'danielm',
+        email: 'danielm@example.com'
+      },
+      {
+        id: 45,
+        name: 'Sophie Wright',
+        username: 'sophiew',
+        email: 'sophiew@example.com'
+      },
+      {
+        id: 46,
+        name: 'Andrew Scott',
+        username: 'andrews',
+        email: 'andrews@example.com'
+      },
+      {
+        id: 47,
+        name: 'Grace Robinson',
+        username: 'gracer',
+        email: 'gracer@example.com'
+      },
+      {
+        id: 48,
+        name: 'Christopher Taylor',
+        username: 'christophert',
+        email: 'christophert@example.com'
+      }*/
+            
+      
+  ];
